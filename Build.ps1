548c4fcc01b28ea1c50268910f061d9bed90d1e4
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-05 12:57:57 +0200

    Use artifacts output for simplifying publishing folder structure

--- Build.ps1
+++ Build.ps1
@@ -26,6 +26,6 @@ $artifacts = ".\artifacts"
 
 if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
 
-exec { & dotnet test -c Release -r $artifacts -l trx --verbosity=normal }
+exec { & dotnet test -c Release -l trx --verbosity=normal }
 
-exec { & dotnet pack .\src\MagicMapper.Extensions.EnumMapping\MagicMapper.Extensions.EnumMapping.csproj -c Release -o $artifacts --no-build }
+exec { & dotnet pack .\src\MagicMapper.Extensions.EnumMapping\MagicMapper.Extensions.EnumMapping.csproj -c Release --no-build }
